cf7a8bb31d1c6c19fb710293d5efeca08209f323
Janos Perge <janos.perge@gmail.com>
2024-01-31 23:22:50 +0100

    minor layout fixing on wallet component

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -11,8 +11,10 @@ function Wallet({ id, balance, name, description }) {
     <Grid item xs={12} lg={3} md={4} sm={6}>
       <Card>
         <CardContent>
-          <Typography variant="h4">{name}</Typography>
-          <Typography variant="subtitle1" gutterBottom>
+          <Typography variant="h4" mb={2}>
+            {name}
+          </Typography>
+          <Typography variant="body2" gutterBottom>
             {description}
           </Typography>
           <Typography variant="subtitle2">Balance: ${balance}</Typography>
