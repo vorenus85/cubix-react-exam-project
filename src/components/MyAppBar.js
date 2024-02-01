fe4dc6bc237d8847d213c5fbb7ca45a728b03fb5
Janos Perge <janos.perge@gmail.com>
2024-02-01 21:10:37 +0100

    extend appbar with wallets and users route

--- src/components/MyAppBar.js
+++ src/components/MyAppBar.js
@@ -1,10 +1,15 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export default function MyAppBar() {
+  const navigate = useNavigate();
+  const navigateToWallets = () => {
+    navigate("/");
+  };
+  const navigateToUsers = () => {
+    navigate("/users");
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -12,6 +17,12 @@ export default function MyAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             MyWallets
           </Typography>
+          <Button onClick={() => navigateToWallets()} color="inherit">
+            Wallets
+          </Button>
+          <Button onClick={() => navigateToUsers()} color="inherit">
+            Users
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
