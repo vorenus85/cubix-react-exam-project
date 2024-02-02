1d90bcba7767c72e3c77f6840a946c48c343f92f
Janos Perge <janos.perge@gmail.com>
2024-02-02 19:48:59 +0100

    add todo

--- src/components/AddWallet.js
+++ src/components/AddWallet.js
@@ -2,6 +2,9 @@ import { Card, CardContent, Typography, Grid } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 function AddWallet({ addNew }) {
+  // Todo use complex button
+  // https://mui.com/material-ui/react-button/#complex-button
+
   return (
     <Grid item xs={12} lg={3} md={4} sm={6}>
       <Card onClick={addNew}>
