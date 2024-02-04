cd1aa730455a5230529f919182e4dbecb74f3421
Janos Perge <janos.perge@gmail.com>
2024-02-04 22:38:39 +0100

    add search panel to users list

--- src/screens/Users.js
+++ src/screens/Users.js
@@ -11,10 +11,14 @@ import {
   Paper,
   Stack,
   Button,
+  Grid,
+  Card,
+  CardContent,
+  TextField,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import { UserButton } from "../components/UserButton";
 import { WalletButton } from "../components/WalletButton";
+import { useState } from "react";
 
 function createData(user, wallets) {
   return { user, wallets };
@@ -81,13 +85,54 @@ const rows = [
 function Users() {
   // TODO use memoization for caching
 
+  const [search, setSearch] = useState();
+
+  const handleSearchUser = (event) => {
+    const searchParam = event.target.value;
+
+    if (searchParam.length > 1) {
+      setSearch(event.target.value);
+      console.log("search user", event.target.value);
+    }
+  };
+
   return (
     <div>
       <MyAppBar />
       <Container maxWidth="md">
-        <Typography variant="h4" my={2} mt={6}>
-          Users list
-        </Typography>
+        <Grid container mb={4} mt={6}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h4">Users list</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Card>
+              <CardContent>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  spacing={2}
+                >
+                  <Stack>
+                    <Typography variant="body1">Search:</Typography>
+                  </Stack>
+                  <Stack>
+                    <TextField
+                      value={search}
+                      name="search"
+                      size="small"
+                      id="outlined-basic"
+                      label="start typing to find user..."
+                      variant="outlined"
+                      onChange={handleSearchUser}
+                    />
+                  </Stack>
+                </Stack>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+
         <TableContainer component={Paper}>
           <Table>
             <TableHead>
