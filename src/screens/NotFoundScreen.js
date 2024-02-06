d8a661cf1ea2e56d2a15e8d2b6517ab2905929d9
Janos Perge <janos.perge@gmail.com>
2024-02-06 23:28:47 +0100

    add appBar to notFoundScreen

--- src/screens/NotFoundScreen.js
+++ src/screens/NotFoundScreen.js
@@ -1,31 +1,35 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Stack, Container } from "@mui/material";
 import "../assets/notFoundScreen.css";
 import { useNavigate } from "react-router-dom";
+import MyAppBar from "../components/MyAppBar";
 
 function NotFoundScreen() {
   const navigate = useNavigate();
 
   return (
-    <div className="error-page">
-      <Typography variant="h1" component="h1" mb={3}>
-        404
-      </Typography>
-      <Typography variant="h4" component="h4" my={3}>
-        Oops! Page not found
-      </Typography>
-      <Typography variant="body2" my={3}>
-        Sorry, the page you're looking for doesn't exist. If you think something
-        is broken, report a problem.
-      </Typography>
-      <Button
-        variant="outlined"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Return to home
-      </Button>
-    </div>
+    <Stack>
+      <MyAppBar />
+      <Container className="error-page" style={{ marginTop: "6rem" }}>
+        <Typography variant="h1" component="h1" mx={3}>
+          404
+        </Typography>
+        <Typography variant="h4" component="h4" my={3}>
+          Oops! Page not found
+        </Typography>
+        <Typography variant="body2" my={3}>
+          Sorry, the page you're looking for doesn't exist. If you think
+          something is broken, report a problem.
+        </Typography>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Return to home
+        </Button>
+      </Container>
+    </Stack>
   );
 }
 
